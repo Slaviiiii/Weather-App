bbc4cc3ec8a2abcb174e0e6d66846afb434645c8
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-08-17 21:37:57 +0300

    update

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-export const GEO_API_URL = 'http://api.weatherapi.com/v1';
-export const WEATHER_API_KEY = '0062ffea6dda492b9c8204406231006';
+export const GEO_API_URL = "https://api.weatherapi.com/v1";
+export const WEATHER_API_KEY = "0062ffea6dda492b9c8204406231006";
--- src/contexts/WeatherContext.js
+++ src/contexts/WeatherContext.js
@@ -1,78 +1,87 @@
-import { createContext, useEffect, useState } from "react";
-import { GEO_API_URL, WEATHER_API_KEY } from '../api';
+import React, { createContext, useEffect, useState } from "react";
+import { GEO_API_URL, WEATHER_API_KEY } from "../api";
 
 export const WeatherContext = createContext();
 
 export const Weather = ({ children }) => {
+  const [search, setSearch] = useState(null);
+  const [hourlyForecast, setHourlyForecast] = useState(null);
+  const [dailyForecast, setDailyForecast] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [search, setSearch] = useState(null);
-    const [hourlyForecast, setHourlyForecast] = useState(null);
-    const [dailyForecast, setDailyForecast] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const url = `${GEO_API_URL}/forecast.json?key=${WEATHER_API_KEY}&q=Sofia`;
+      const response = await fetch(url);
+      const data = await response.json();
 
-    useEffect(() => {
-        ((async () => {
-                const sofiaHourlyForecastRes = await fetch(`${GEO_API_URL}/forecast.json?key=${WEATHER_API_KEY}&q=Sofia`);
-                const sofiaHourlyForecast = await sofiaHourlyForecastRes.json();
-                const sofiaDailyForecastRes = await fetch(`${GEO_API_URL}/forecast.json?key=${WEATHER_API_KEY}&q=Sofia&days=8`);
-                const sofiaDailyForecast = await sofiaDailyForecastRes.json();
+      setHourlyForecast({ city: "Sofia, Bulgaria", ...data });
 
-                setHourlyForecast({ city: 'Sofia, Bulgaria', ...sofiaHourlyForecast });
-                setDailyForecast({ city: 'Sofia, Bulgaria', ...sofiaDailyForecast });
-                setIsLoading(false);
-        }))()
-    }, [])
+      const dailyUrl = `${GEO_API_URL}/forecast.json?key=${WEATHER_API_KEY}&q=Sofia&days=8`;
+      const dailyResponse = await fetch(dailyUrl);
+      const dailyData = await dailyResponse.json();
 
-    const loadOptions = async (inputValue) => {
-        try {
-            const response = await fetch(`${GEO_API_URL}/search.json?key=${WEATHER_API_KEY}&q=${inputValue ? inputValue : 'Sofia'}`); 
-            const res = await response.json();
-            return {
-                options: res.map(x => {
-                    return {
-                        label: `${x.name}, ${x.country}`
-                    }
-                })
-            }
-        }
-        catch (err) {
-            console.log(err.message);
-        }
+      setDailyForecast({ city: "Sofia, Bulgaria", ...dailyData });
+
+      setIsLoading(false);
+    })();
+  }, []);
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/search.json?key=${WEATHER_API_KEY}&q=${
+          inputValue ? inputValue : "Sofia"
+        }`
+      );
+      const res = await response.json();
+      return {
+        options: res.map((x) => ({
+          label: `${x.name}, ${x.country}`,
+        })),
+      };
+    } catch (err) {
+      console.log(err.message);
     }
+  };
 
-    const onSearchChange = async (searchData) => {
-        const [city, country] = searchData.label.split(', ');
+  const onSearchChange = async (searchData) => {
+    const [city, country] = searchData.label.split(", ");
 
-            const hourlyForecastRes = await fetch(`${GEO_API_URL}/forecast.json?key=${WEATHER_API_KEY}&q=${city}`);
-            const hourlyForecastData = await hourlyForecastRes.json();
-            const dailyForecastRes = await fetch(`${GEO_API_URL}/forecast.json?key=${WEATHER_API_KEY}&q=${city}&days=8`);
-            const dailyForecastData = await dailyForecastRes.json();
+    const hourlyForecastRes = await fetch(
+      `${GEO_API_URL}/forecast.json?key=${WEATHER_API_KEY}&q=${city}`
+    );
+    const hourlyForecastData = await hourlyForecastRes.json();
 
-            setHourlyForecast({ city: `${city}, ${country}`, ...hourlyForecastData });
-            setDailyForecast({ city: `${city}, ${country}`, ...dailyForecastData })
-        setIsLoading(false);
-    }
+    const dailyForecastRes = await fetch(
+      `${GEO_API_URL}/forecast.json?key=${WEATHER_API_KEY}&q=${city}&days=8`
+    );
+    const dailyForecastData = await dailyForecastRes.json();
 
-    const handleOnChange = (searchValue) => {
-        setSearch(searchValue);
-        onSearchChange(searchValue);
-    }
+    setHourlyForecast({ city: `${city}, ${country}`, ...hourlyForecastData });
+    setDailyForecast({ city: `${city}, ${country}`, ...dailyForecastData });
 
-    const contextValues = {
-        dailyForecast,
-        hourlyForecast,
-        handleOnChange,
-        onSearchChange,
-        loadOptions,
-        search,
-        isLoading,
-    }
+    setIsLoading(false);
+  };
 
-    return (
-        <>
-            <WeatherContext.Provider value={contextValues}>
-                {children}
-            </WeatherContext.Provider>
-        </>
-    )
-}
+  const handleOnChange = (searchValue) => {
+    setSearch(searchValue);
+    onSearchChange(searchValue);
+  };
+
+  const contextValues = {
+    dailyForecast,
+    hourlyForecast,
+    handleOnChange,
+    onSearchChange,
+    loadOptions,
+    search,
+    isLoading,
+  };
+
+  return (
+    <WeatherContext.Provider value={contextValues}>
+      {children}
+    </WeatherContext.Provider>
+  );
+};
